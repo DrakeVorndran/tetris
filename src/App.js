38dc133898baf74fbaab4aacf5034a23f813e0b1
Drake Vorndran <DrakeVorndranOffical@gmail.com>
2019-04-04 14:12:45 -0700

    added grid square

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import GridSquare from './components/grid-square'
+
 class App extends Component {
   render() {
     return (
@@ -8,6 +10,7 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Tetris Redux</h1>
         </header>
+        <GridSquare color="1" />
       </div>
     );
   }
