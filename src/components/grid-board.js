ae67705c67af429ee171cab32ea88233244cfade
Drake Vorndran <DrakeVorndranOffical@gmail.com>
2019-04-04 15:13:13 -0700

    added connection for grid board

--- src/components/grid-board.js
+++ src/components/grid-board.js
@@ -1,18 +1,46 @@
 import React, { Component } from 'react'
 import GridSquare from './grid-square'
 
+import { moveDown } from '../actions'
+import { shapes } from '../utils'
+
+import { connect } from 'react-redux'
+
 class GridBoard extends Component {
 
   makeGrid() {
-    const grid = []
-    for (let row = 0; row < 18; row++ ) {
-      grid.push([])
-      for (let col = 0; col < 10; col++ ) {
-        grid[row].push(<GridSquare key={`${col},${row}`} color="1" />)
-      }
-    }
+    const { grid, shape, rotation, x, y } = this.props
+    const block = shapes[shape][rotation]
+    const blockColor = shape
 
-    return grid
+    return grid.map((rowArray, row) => {
+      return rowArray.map((square, col) => {
+        const blockX = col - x
+        const blockY = row - y
+        let color = square
+        if (blockX >= 0 && blockX < block.length && blockY >= 0 && blockY < block.length) {
+          color = block[blockY][blockX] === 0 ? color : blockColor
+        }
+        const k = row * grid[0].length + col;
+        return ( <GridSquare
+              key={k}
+              square={square}
+              color={color}>{square}
+            </GridSquare> )
+      })
+    })
+
+
+
+
+    // for (let row = 0; row < 18; row++ ) {
+    //   grid.push([])
+    //   for (let col = 0; col < 10; col++ ) {
+    //     grid[row].push(<GridSquare key={`${col},${row}`} color="1" />)
+    //   }
+    // }
+
+    // return grid
   }
 
   render() {
@@ -24,4 +52,22 @@ class GridBoard extends Component {
   }
 }
 
-export default GridBoard
+const mapStateToProps = (state) => {
+  return {
+    grid: state.game.grid,
+    shape: state.game.shape,
+    rotation: state.game.rotation,
+    x: state.game.x,
+    y: state.game.y,
+    speed: state.game.speed,
+    isRunning: state.game.isRunning
+  }
+}
+
+const mapDispatchToProps = () => {
+  return{
+    moveDown
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps())(GridBoard)
