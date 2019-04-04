66c53343a673acf08caee3d552e52299551551db
Drake Vorndran <DrakeVorndranOffical@gmail.com>
2019-04-04 14:32:51 -0700

    added initial message popup

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import NextBlock from './components/next-block'
 import GridBoard from './components/grid-board'
 import ScoreBoard from './components/score-board'
 import Controls from './components/controls'
+import MessagePopup from './components/message-popup'
 
 
 
@@ -19,6 +20,7 @@ class App extends Component {
         <NextBlock />
         <ScoreBoard />
         <Controls />
+        <MessagePopup />
       </div>
     );
   }
