866af935bb78937b9e3ca5305adfaeebd48e4617
Ki Vorndran <drake.vorndran@students.makeschool.com>
2019-08-28 11:07:02 -0700

    Updates

---
+++ precache-manifest.431c9a1c3a2a2a5543c272ee78dc5803.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f70876e86bfd50e4545c",
+    "url": "/tetris/static/css/main.0bace1a1.chunk.css"
+  },
+  {
+    "revision": "f70876e86bfd50e4545c",
+    "url": "/tetris/static/js/main.50438e3b.chunk.js"
+  },
+  {
+    "revision": "e9e58439d3b8c64431c2",
+    "url": "/tetris/static/js/runtime~main.61707ba2.js"
+  },
+  {
+    "revision": "9cbc7ec7344c99039a12",
+    "url": "/tetris/static/js/2.d997cfd3.chunk.js"
+  },
+  {
+    "revision": "d751b1c3309adbd3a1252b12cce98c5d",
+    "url": "/tetris/index.html"
+  }
+];
